0e8c3efe20739627a43895254ebd5fc7f6c3397d
xfxx20222 <1255104596@qq.com>
2023-07-15 23:05:01 +0800

    更新 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [solidJs(), tailwind(), react()],
   output: 'server',
   adapter: process.env.VERCEL
-     vercel({ analytics: true })
+ analytics: true,
     : node({ mode: 'standalone' }),
   vite: {
     plugins: [

1e4c2b96000b44bf4b9d00bcf99b4b0d108a5d27
xfxx20222 <1255104596@qq.com>
2023-07-15 22:50:40 +0800

    更新 index.tsx

--- src/modules/Sidebar/index.tsx
+++ src/modules/Sidebar/index.tsx
@@ -81,7 +81,7 @@ const Sidebar: FC<{
   return (
     <div className="flex flex-col h-full bg-white">
       <div className="p-4 mt-2 flex items-baseline justify-between">
-        <span className="text-3xl text-gradient font-[800]">番茄酱的ChatGPT</span>
+        <span className="text-3xl text-gradient font-[800]">ChatGPT</span>
         <a
           href="https://blog.aidengrong.top/link"
           target="_blank"

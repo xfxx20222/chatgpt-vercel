74849c5b624abf725c42f290c8d307cb8a3d569e
xfxx20222 <1255104596@qq.com>
2023-07-16 10:06:42 +0800

    更新 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import checker from 'vite-plugin-checker';
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
